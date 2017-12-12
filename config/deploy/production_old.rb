d5a574d5b1bfd3920f409b7c0429963c1eec6b4c
janslonecki <janslonecki@gmail.com>
2017-12-12 15:24:49 +0100

    RP-5512 - deployment config

--- config/deploy/production_old.rb
+++
@@ -1,6 +0,0 @@
-set :stage, :production
-server '46.248.188.98', user: 'zapytaj', port: '1986', roles: %w(app db web notify)
-set :deploy_to, "/var/www/zapytaj"
-set :user, 'zapytaj'
-
-ask :branch, proc { `git describe --tags $(git rev-list --tags --max-count=1)`.chomp }
